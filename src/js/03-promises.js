36164b956f57cb507c7d7b49fe176c264c0356cf
Nazariy Bakush <nazkamaz@gmail.com>
2022-01-17 13:43:59 +0200

    hw9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('[type="submit"]');
+const form = document.querySelector('.form');
+
+startBtn.addEventListener('click', onBtnSbm);
+
+function onBtnSbm(elm) {
+  elm.preventDefault();
+  let position = 0;
+  let delay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+
+  setTimeout(() => {
+    setInterval(() => {
+      if (position >= amount) {
+        return;
+      }
+      position += 1;
+      delay += step;
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, step);
+  }, delay);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    }
+    reject({ position, delay });
+  });
+  return promise;
 }
